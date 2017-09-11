5bc9f9f43f1a3bf32f7eff8370725652a013d332
ChengqianLyu <lyuc@rose-hulman.edu>
2017-09-11 12:52:42 -0400

    name

--- src/problem0.py
+++ src/problem0.py
@@ -147,7 +147,7 @@ def problem0a(n):
            since (2 + 4 + 6) is 12, which is NOT odd.
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -164,7 +164,11 @@ def problem0a(n):
     #        Simply try a few examples to convince yourself of this.
     #        ASK FOR HELP if you do not understand this hint.
     # ------------------------------------------------------------------
-
+    a = sum_of_digits(n)
+    if a % 2 == 0:
+        return False
+    else:
+        return True
 
 def test_problem0b():
     """ Tests the   problem0b   function. """
@@ -219,7 +223,7 @@ def problem0b(n):
            since there are 46 primes between 2 and 200.
      """
     # ------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DONE: 3. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -228,8 +232,12 @@ def problem0b(n):
     #    **  use (call) the   is_prime   function that is DEFINED ABOVE.
     ####################################################################
     # ------------------------------------------------------------------
-
-
+    c = 0
+    for k in range(2,n+1):
+        b = is_prime(k)
+        if b is True:
+            c = c + 1
+    return c
 def test_problem0c():
     """ Tests the   problem0c  function. """
     print()
@@ -287,7 +295,7 @@ def problem0c(circle, n, window):
       :type window: rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 4. Implement and test this function.
+    # DONE: 4. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
@@ -295,7 +303,17 @@ def problem0c(circle, n, window):
     #   renders with a half-second pause after rendering.
     ####################################################################
     # ------------------------------------------------------------------
-
+    a = circle
+    a.attach_to(window)
+    r = a.radius
+    x1 = a.center.x + 2 * r
+    y1 = a.center.y
+    for k in range(n):
+        c = rg.Circle(rg.Point(x1, y1), r)
+        x1 = x1 + 2 * r
+        c.attach_to(window)
+        window.render(0.5)
+    window.close_on_mouse_click()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
